b55d79468b9d09932afc9a42c42713c75b140fd4
Manar Monzer <mamonzer@2-A-2.42heilbronn.de>
2026-02-20 15:52:28 +0100

    done

---
+++ ex1/data_stream.py
@@ -0,0 +1,38 @@
+
+from abc import ABC, abstractmethod
+
+
+from typing import Any, List, Dict, Union, Optional
+
+class DataStream(ABC):
+
+    @abstractmethod
+    def process_batch(self, data_batch: List[Any]) -> str:
+        pass
+
+    @abstractmethod
+    def filter_data(self, data_batch: List[Any], criteria: Optional[str] = None) -> List[Any]:
+        pass
+
+    @abstractmethod
+    def get_status(self) -> Dict[str, Union[str, int, float]]:
+        pass
+
+
+
+
+
+
+class SensorStream(DataStream):
+    pass
+
+
+
+
+
+class TransactionStream(DataStream):
+    pass
+
+
+class EventStream(DataStream):
+    pass
